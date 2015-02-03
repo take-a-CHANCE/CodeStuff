f50ad058b9823f7c19447af781e6ffc22a300304
take-a-CHANCE <chancejd53@gmail.com>
2015-02-03 10:11:58 -0500

    Syncing STuff

--- School/HW/src/ProgramSkeleton.java
+++ School/HW/src/ProgramSkeleton.java
@@ -61,5 +61,47 @@ public final class ProgramSkeleton {
             q.enqueue(temp.dequeue());
         }
     }
+    
+    /**
+     * Finds pair with first component {@code key} and, if such exists, moves it
+     * to the front of {@code q}.
+     *
+     * @param <K>
+     *            type of {@code Pair} key
+     * @param <V>
+     *            type of {@code Pair} value
+     * @param q
+     *            the {@code Queue} to be searched
+     * @param key
+     *            the key to be searched for
+     * @updates q
+     * @ensures <pre>
+     * perms(q, #q)  and
+     * if there exists value: V (<(key, value)> is substring of q)
+     *  then there exists value: V (<(key, value)> is prefix of q)
+     * </pre>
+     */
+    private static <K, V> void moveToFront(Queue<Pair<K, V>> q, K key) {
+        boolean containsX = false;
+        Queue<Pair<K, V>> temp = q.newInstance();
+        Pair<K, V> p = new SimplePair<>();
+
+        for (int i = 0; i < q.length() && !containsX; i++) {
+            Pair<K, V> test = q.dequeue();
+            if (test.key().equals(key)) {
+                containsX = true;
+                Pair<K, V> p = new SimplePair<>(key, test.value());
+            } else {
+                temp.enqueue(test);
+            }
+        }
+        if (containsX && q.length() > 0) {
+            Pair<K, V> front = q.replaceFront(p);
+            temp.enqueue(front);
+        } else if (containsX) {
+            q.enqueue(p);
+        }
+        q.append(temp);
+    }
 
 }
