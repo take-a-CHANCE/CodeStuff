d4ff3fa9a4361755434c44c3947fb7d4c6d0b7d6
take-a-CHANCE <chancejd53@gmail.com>
2015-02-05 10:08:58 -0500

    Implemented mod for HW

--- School/HW/src/HelloWorld.java
+++ School/HW/src/HelloWorld.java
@@ -27,4 +27,27 @@ public final class HelloWorld {
         out.close();
     }
 
+    /**
+     * Computes {@code a} mod {@code b} as % should have been defined to work.
+     *
+     * @param a
+     *            the number being reduced
+     * @param b
+     *            the modulus
+     * @return the result of a mod b, which satisfies 0 <= {@code mod} < b
+     * @requires b > 0
+     * @ensures <pre>
+     * 0 <= mod  and  mod < b  and
+     * there exists k: integer (a = k * b + mod)
+     * </pre>
+     */
+    public static int mod(int a, int b) {
+        if (a >= 0) {
+            return a % b;
+        } else {
+            int mult = a / b;
+            int toMove = a - (a * mult);
+            return -1 * (b - a);
+        }
+    }
 }
