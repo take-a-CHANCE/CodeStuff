df2def9e2e51fad1665ddc81198a41826df9e9fe
take-a-CHANCE <chancejd53@gmail.com>
2015-02-12 10:23:04 -0500

    binaryTree isInTree method

--- School/HW/src/ProgramWithIOAndStaticMethod.java
+++ School/HW/src/ProgramWithIOAndStaticMethod.java
@@ -1,3 +1,4 @@
+import components.binarytree.BinaryTree;
 import components.simplereader.SimpleReader;
 import components.simplereader.SimpleReader1L;
 import components.simplewriter.SimpleWriter;
@@ -44,6 +45,36 @@ public final class ProgramWithIOAndStaticMethod {
          */
         in.close();
         out.close();
+    }
+
+    /**
+     * Returns whether {@code x} is in {@code t}.
+     *
+     * @param <T>
+     *            type of {@code BinaryTree} labels
+     * @param t
+     *            the {@code BinaryTree} to be searched
+     * @param x
+     *            the label to be searched for
+     * @return true if t contains x, false otherwise
+     * @requires IS_BST(t)
+     * @ensures isInTree = (x is in labels(t))
+     */
+    public static <T extends Comparable<T>> boolean isInTree(BinaryTree<T> t,
+            T x) {
+        boolean found = false;
+        BinaryTree<T> left = t.newInstance();
+        BinaryTree<T> right = t.newInstance();
+        T root = t.disassemble(left, right);
+        if (root.equals(x)) {
+            found = true;
+        } else if (x.compareTo(t.root()) < 0) {
+            found = isInTree(left, x);
+        } else {
+            found = isInTree(right, x);
+        }
+        t.assemble(root, left, right);
+        return found;
     }
 
 }
